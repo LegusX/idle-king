f0523f84ac08173ce45914975e1a806b6a1c1fd2
LegusX <loghenrie@gmail.com>
2017-11-20 09:29:58 -0600

    might've forgot these ones

--- tabchanger.js
+++ tabchanger.js
@@ -1,25 +1,31 @@
 const tabchanger = {
-    homeChanger: function() {
-        var div = document.getElementById("mainhome");
-        if (!div.style.display === "none") return;
-        div.style.display === "";
-        document.getElementById("mainresearch").style.display = "none";
-        document.getElementById("mainworkers").style.display = "none";
-    },
-
-    researchChanger: function() {
-        var div = document.getElementById("mainresearch");
-        if (!div.style.display === "none") return;
-        div.style.display === "";
-        document.getElementById("mainhome").style.display = "none";
-        document.getElementById("mainworkers").style.display = "none";
-    },
-
-    workChanger: function() {
-        var div = document.getElementById("mainworkers");
-        if (!div.style.display === "none") return;
-        div.style.display === "";
-        document.getElementById("mainresearch").style.display = "none";
-        document.getElementById("mainhome").style.display = "none";
-    }
-};
+	homeChanger: function() {
+		try{
+			var div = document.getElementById("mainhome")
+			if(div.style.display !== "none") return;
+			div.style.display = ""
+			document.getElementById("mainresearch").style.display = "none"
+			document.getElementById("mainworkers").style.display = "none"
+		}catch(e){console.log(e)}
+	},
+	
+	researchChanger: function() {
+		try{
+			var div = document.getElementById("mainresearch")
+			if(div.style.display !== "none") return;
+			div.style.display = ""
+			document.getElementById("mainhome").style.display = "none"
+			document.getElementById("mainworkers").style.display = "none"
+		}catch(e){console.log(e)}
+	},
+	
+	workersChanger: function() {
+		try {
+			var div = document.getElementById("mainworkers")
+			if(div.style.display !== "none") return;
+			div.style.display = ""
+			document.getElementById("mainresearch").style.display = "none"
+			document.getElementById("mainhome").style.display = "none"
+		}catch(e){console.log(e)}
+	}
+}
